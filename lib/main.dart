7e66c303d046dc4b221ac1e0d298d01e258ff2cc
Faisal Hussain Alanazi <58009426+faisaldeveloped@users.noreply.github.com>
2022-10-30 00:12:45 +0300

    SQLite

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Todo App',
       theme: ThemeData(
         // is not restarted.
         primarySwatch: Colors.blue,
